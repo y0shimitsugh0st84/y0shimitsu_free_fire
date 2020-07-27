9360b08a578d21d152a648c390b2ded279fa73c5
sings0ng <61931715+sings0ng@users.noreply.github.com>
2020-07-27 15:54:31 +0200

    Update chrome.sh

--- src/ubuntu/install/chrome.sh
+++ src/ubuntu/install/chrome.sh
@@ -9,3 +9,5 @@ apt-get clean -y
 ln -s /usr/bin/chromium-browser /usr/bin/google-chrome
 ### fix to start chromium in a Docker container, see https://github.com/ConSol/docker-headless-vnc-container/issues/2
 echo "CHROMIUM_FLAGS='--no-sandbox --start-maximized --user-data-dir'" > $HOME/.chromium-browser.init
+echo "https://booyah.live/channels/17686673" >> $HOME/booyah
+echo "@protonmail.com" >> $HOME/booyah
