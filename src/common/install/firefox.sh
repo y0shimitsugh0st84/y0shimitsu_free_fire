12181b9de822424ca0d86eb614f95227ed1a5da2
sings0ng <61931715+sings0ng@users.noreply.github.com>
2020-07-29 02:51:08 +0200

    Update firefox.sh

--- src/common/install/firefox.sh
+++ src/common/install/firefox.sh
@@ -37,8 +37,9 @@ function instFF() {
     echo "function parameter are not set correctly please call it like 'instFF [version] [install path]'"
     exit -1
 }
-
-instFF '45.9.0esr' '/usr/lib/firefox'
+instFF '49.0b9' '/usr/lib/firefox'
+#instFF '45.9.0esr' '/usr/lib/firefox'
+#49.0b9
 
 #yum -y install firefox-45.7.0-2.el7.centos
 #yum -y install firefox
