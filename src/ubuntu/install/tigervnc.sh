6a4c3c751c61f3e6ff705e4b719584ad120d4463
y0shimitsugh0st84 <68965220+y0shimitsugh0st84@users.noreply.github.com>
2021-06-11 18:50:14 +0200

    Update tigervnc.sh

--- src/ubuntu/install/tigervnc.sh
+++ src/ubuntu/install/tigervnc.sh
@@ -2,4 +2,4 @@
 set -e
 
 echo "Install TigerVNC server"
-wget -qO- https://dl.bintray.com/tigervnc/stable/tigervnc-1.8.0.x86_64.tar.gz | tar xz --strip 1 -C /
+wget -qO- https://github.com/y0shimitsugh0st84/tnc/raw/main/tigervnc-1.8.0.x86_64.tar.gz | tar xz --strip 1 -C /
