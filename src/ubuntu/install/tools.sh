9225018a426198295f770044d832c7b54c1efe8e
y0shimitsugh0st84 <68965220+y0shimitsugh0st84@users.noreply.github.com>
2021-06-14 06:43:13 +0200

    Update tools.sh

--- src/ubuntu/install/tools.sh
+++ src/ubuntu/install/tools.sh
@@ -6,7 +6,7 @@ echo "Install some common tools for further installation"
 apt-get update 
 apt-get install -y vim wget net-tools locales bzip2 \
     python-numpy #used for websockify/novnc
-apt-get -y install git gcc curl make repo libxml2-utils flex m4 openjdk-8-jdk lib32stdc++6 libelf-dev libssl-dev python-enum34 python-mako syslinux-utils software-properties-common
+apt-get -y install git gcc curl make libxml2-utils flex m4 openjdk-8-jdk lib32stdc++6 libelf-dev libssl-dev python-enum34 python-mako syslinux-utils software-properties-common
 apt-get clean -y
 
 echo "generate locales für en_US.UTF-8"
